c1feaded9540b1777d764c17c40f4678b5164ad3
inessmelo <prof.inesmelo@gmail.com>
2025-10-28 23:00:31 -0300

    Inserindo Stages com Duration e Target no script de teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -3,8 +3,15 @@ import { sleep, check } from 'k6'
 
 export const options = {
     //iterations: 40,
-    vus: 15,
-    duration: '30s',
+    //vus: 15,
+    //duration: '30s',
+    
+    stages: [
+        { duration: '5s', target: 10},
+        { duration: '20s', target: 10},
+        { duration: '5s', target: 0},
+    ],
+
     thresholds: {
         http_req_duration: ['p(90)<3', 'p(95)<4']
     }
